bd730877902546ebe2fffdcd92bdfc8e13d3d21a
OscarThroedsson <oscar.throedsson@gmail.com>
2025-01-23 22:08:46 +0100

    added images of the teachers and created teacher obj

---
+++ client/src/assets/techers.ts
@@ -0,0 +1,87 @@
+export const teachers = [
+  {
+    name: "Kim",
+    subject: "matematik",
+    image: "",
+  },
+  {
+    name: "Helena",
+    subject: "svenska",
+    image: "",
+  },
+  {
+    name: "Tara",
+    subject: "engelska",
+    image: "",
+  },
+  {
+    name: "Martin",
+    subject: "historia",
+    image: "",
+  },
+  {
+    name: "Monika",
+    subject: "geografi",
+    image: "",
+  },
+  {
+    name: "Henrik",
+    subject: "biologi",
+    image: "",
+  },
+  {
+    name: "Albert",
+    subject: "fysik",
+    image: "",
+  },
+  {
+    name: "Jonas",
+    subject: "kemi",
+    image: "",
+  },
+  {
+    name: "Camilla",
+    subject: "samhällskunskap",
+    image: "",
+  },
+  {
+    name: "",
+    subject: "religionskunskap",
+    image: "",
+  },
+  {
+    name: "Oscar",
+    subject: "idrott och hälsa",
+    image: "",
+  },
+  {
+    name: "Alf",
+    subject: "teknik",
+    image: "",
+  },
+  {
+    name: "Daniel",
+    subject: "musik",
+    image: "",
+  },
+  {
+    name: "Joakim",
+    subject: "bild",
+    image: "",
+  },
+  {
+    name: "Bo",
+    subject: "slöjd",
+    image: "",
+  },
+  {
+    name: "Ebba",
+    subject: "naturkunskap",
+    image: "",
+  },
+  {
+    name: "",
+    subject: "kunskapsprov",
+    image: "",
+  },
+];
