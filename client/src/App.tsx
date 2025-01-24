f73ce74c2b88496095378fc82419cb083eafcb77
OscarThroedsson <oscar.throedsson@gmail.com>
2025-01-24 23:00:51 +0100

    added all teachers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,20 @@
 import "@/index.css";
 import StudentHeader from "./components/StudentHeader";
 import TeacherCard from "./components/TeacherCard";
+import { teachers } from "./assets/techers";
 
 function App() {
   return (
     <>
       <div>Lexter</div>
       <div className="w-full flex items-center justify-center mx-auto flex-col">
-        <div>
+        <div className="space-y-6">
           <StudentHeader />
-          <TeacherCard />
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {teachers.map((teacher) => (
+              <TeacherCard teacher={teacher} />
+            ))}
+          </div>
         </div>
       </div>
     </>
--- client/src/components/TeacherCard.tsx
+++ client/src/components/TeacherCard.tsx
@@ -1,11 +1,30 @@
 import fitness from "@/assets/images/fitness.jpg";
 import { Card } from "@/components/ui/card";
-export default function TeacherCard() {
+
+interface TeacherCard_Props {
+  teacher: {
+    name: string;
+    subject: string;
+    image: string;
+  };
+}
+
+export default function TeacherCard({ teacher }: TeacherCard_Props) {
   return (
-    <Card className="h-60 w-52 bg-cover bg-center overflow-hidden flex " style={{ backgroundImage: `url(${fitness})` }}>
-      <div className="bg-slate-100/60 p-2 mt-auto w-full">
-        <p className="font-bold text-center ">Idrott & Hälsa</p>
-      </div>
-    </Card>
+    <div className="group hover:scale-105 transition-all">
+      <p className="text-xs font-semibold pl-2">{teacher.name}</p>
+      <Card
+        className="min-h-60 w-full bg-cover bg-no-repeat bg-center overflow-hidden flex rounded-m border-separate border-transparent 
+        border-2 group-hover:border-green-500 group-hover:shadow-md
+        active:scale-95 active:shadow-inner
+        transformation-all duration-200 
+        "
+        style={{ backgroundImage: `url(${teacher.image || fitness})` }}
+      >
+        <div className="bg-slate-100/80 p-2 mt-auto w-full">
+          <p className="font-bold text-center">{teacher.subject}</p>
+        </div>
+      </Card>
+    </div>
   );
 }
